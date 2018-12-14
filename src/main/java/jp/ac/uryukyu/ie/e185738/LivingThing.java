63675ab14ba81a888cc6d430c04157f1b506bc7a
Rei Yamashiro <e185738@ie.u-ryukyu.ac.jp>
2018-12-14 16:15:19 +0900

    step1 commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e185738/LivingThing.java
@@ -0,0 +1,65 @@
+package jp.ac.uryukyu.ie.e185738;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int maximumHP, int attack){
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        if (dead == false) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damege){
+        setHitPoint( getHitPoint() - damege );
+        if (getHitPoint() <= 0){
+            setDead( true );
+            System.out.printf("%sは倒れた。\n", getName());
+        }
+
+    }
+
+    public int getHitPoint(){
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+
+    public int getAttack(){
+        return attack;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    public boolean getDead(){
+        return dead;
+    }
+
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+}
